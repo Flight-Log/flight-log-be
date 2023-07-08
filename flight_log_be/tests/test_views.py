2c72c39186d30360ab617369592c741db8ae6da6
Katie Lonsdale <katie952@gmail.com>
2023-07-08 16:05:09 -0600

    test: add test set up for get user endpoint

---
+++ flight_log_be/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from flight_log_be.models import User
+from django.test import Client
+from .factories import UserFactory 
+
+class GetUserCase(TestCase):
+    def setUp(self):
+        users = UserFactory.create_batch(10)
+        global user
+        user = User.objects.last()
+
+    def test_user_is_returned(self):
+        c = Client()
+        response = c.get(f'/api/v1/users/{user.id}')
