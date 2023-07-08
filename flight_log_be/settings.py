95a2219ff71f3868dd97650b81d2ea93315f38ab
Katie Lonsdale <katie952@gmail.com>
2023-07-08 13:01:16 -0600

    take out psycopg

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'flight_log_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'FlightLog',
         'USER': 'katielonsdale',
         'PASSWORD': '1234',
