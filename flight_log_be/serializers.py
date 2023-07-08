bf24cc1d88a226499ab1e24ef148d12c53993d62
Katie Lonsdale <katie952@gmail.com>
2023-07-08 10:04:10 -0600

    feat: add flight serializer

--- flight_log_be/serializers.py
+++ flight_log_be/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import User
+from .models import Flight
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name']
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = ['id', 'user', 'date', 'start_location', 'end_location', 'day_hours', 'night_hours', 'aircraft', 'description', 'role']
