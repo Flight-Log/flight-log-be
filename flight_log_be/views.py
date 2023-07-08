03c4b4d12a58b87d3366b9800ec68aefe394d146
Katie Lonsdale <katie952@gmail.com>
2023-07-08 16:54:11 -0600

    wip: list a user class

--- flight_log_be/views.py
+++ flight_log_be/views.py
@@ -1,9 +1,18 @@
 from django.http import JsonResponse
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 def user_list(request):
   
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
   return JsonResponse(serializer.data, safe=False)
+
+
+class ListAUser(APIView):
+
+  def get(request, id):
+    user = User.objects.get(id=id)
+    return Response(status=200)
