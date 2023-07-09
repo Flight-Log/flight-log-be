dfa0510f47edac89e1437df570aa2e1d9d1b22b3
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-09 16:06:38 -0400

    chore: add import statements to views.py

--- flight_log_be/views.py
+++ flight_log_be/views.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
 from .models import User
+from .models import Flight
 from .serializers import UserSerializer
+from .serializers import FlightSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 def user_list(request):
-  
-  users = User.objects.all()
-  serializer = UserSerializer(users, many=True)
-  return JsonResponse(serializer.data, safe=False)
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return JsonResponse(serializer.data, safe=False)
