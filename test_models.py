2cc739a8486a056e0622e44ae96bde60d9e55cba
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-07-10 01:22:52 -0400

    test: create model tests

---
+++ test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.utils import timezone
+from flight_log_be.models import User, Flight
+
+class FlightModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(first_name='John', last_name='Doe')
+        self.flight = Flight.objects.create(
+            user=self.user,
+            date=timezone.now().date(),
+            start_location='City A',
+            end_location='City B',
+            day_hours=8.5,
+            night_hours=2.5,
+            aircraft='Boeing 747',
+            description='Flight from City A to City B.',
+            role='Pilot'
+        )
+    
+    def test_flight_model_representation(self):
+        self.assertEqual(
+            str(self.flight),
+            'City A to City B'
+        )
+
+    def test_flight_model_attributes(self):
+        self.assertEqual(self.flight.user, self.user)
+        self.assertEqual(self.flight.date, timezone.now().date())
+        self.assertEqual(self.flight.start_location, 'City A')
+        self.assertEqual(self.flight.end_location, 'City B')
+        self.assertEqual(self.flight.day_hours, 8.5)
+        self.assertEqual(self.flight.night_hours, 2.5)
+        self.assertEqual(self.flight.aircraft, 'Boeing 747')
+        self.assertEqual(self.flight.description, 'Flight from City A to City B.')
+        self.assertEqual(self.flight.role, 'Pilot')
+        self.assertEqual(self.flight.user.first_name, 'John')
+        self.assertEqual(self.flight.user.last_name, 'Doe')
