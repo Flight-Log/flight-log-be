9a3f335611801db8d4dd5ab18897691839f41290
Katie Lonsdale <katie952@gmail.com>
2023-07-10 14:39:20 -0600

    test: add get a user endpoint test

--- flight_log_be/tests/test_views.py
+++ flight_log_be/tests/test_views.py
@@ -13,7 +13,11 @@ class GetUserCase(TestCase):
     def test_user_is_returned(self):
         c = Client()
         response = c.get(f'/api/v1/users/{user.id}')
-
         self.assertEqual(response.status_code, 200)
         
-        json = json.loads(response)
+        returned_user = json.loads(response.content)["data"]
+
+        self.assertEqual(returned_user["id"], f"{user.id}")
+        self.assertEqual(returned_user["type"], "user")
+        self.assertEqual(returned_user["attributes"]["first_name"], user.first_name )
+        self.assertEqual(returned_user["attributes"]["last_name"], user.last_name )
