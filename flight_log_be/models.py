3f88688b0379a2bd268b3576278830db78c8b916
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-07-10 01:22:37 -0400

    update DateField to default to timezone

--- flight_log_be/models.py
+++ flight_log_be/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
   first_name = models.CharField(max_length=50)
@@ -8,15 +9,15 @@ class User(models.Model):
     return str(self.first_name + " " + self.last_name)
 
 class Flight(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date = models.DateField(max_length=50)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_flights')
+  date = models.DateField(default=timezone.now)
   start_location = models.CharField(max_length=50)
   end_location = models.CharField(max_length=50)
-  day_hours = models.FloatField(max_length=50)
-  night_hours = models.FloatField(max_length=50)
+  day_hours = models.DecimalField(max_digits=4, decimal_places=2)
+  night_hours = models.DecimalField(max_digits=4, decimal_places=2)
   aircraft = models.CharField(max_length=50)
   description = models.CharField(max_length=500)
   role = models.CharField(max_length=50)
 
   def __str__(self):
-    return str(self.start_location + " to " + self.end_location)
+    return '{} to {}'.format(self.start_location, self.end_location)
