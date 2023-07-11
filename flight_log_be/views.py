75116beacaf79c103efc237e1f3512c7a1ef4ab0
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-11 16:48:54 -0400

    chore: add get_flights_for_user function and include commented-out code for JSON API class-based view approach for reference only

--- flight_log_be/views.py
+++ flight_log_be/views.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+# from rest_framework.views import APIView --> needed for class-based views if using JSON-API
+
 
 def user_list(request):
     users = User.objects.all()
@@ -15,13 +17,42 @@ def user_list(request):
 
 
 @api_view(["GET"])
-def flight_list(request, id):
+def get_flights_for_user(request, id):
     try:
         user = User.objects.get(pk=id)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    except:
+        error = {"errors": [{"detail": "Invalid user id."}]}
+        return JsonResponse(error, status=404)
 
-    if request.method == "GET":
-        flights = Flight.objects.filter(user=user)
-        serializer = FlightSerializer(flights, many=True)
-        return Response(serializer.data)
+    flights = Flight.objects.filter(user=user)
+    serializer = FlightSerializer(flights, many=True)
+    flight_details = []
+
+    for index, x in enumerate(flights):
+        flight = {
+            "id": f"{x.id}",
+            "type": "flight",
+            "attributes": serializer.data[index],
+        }
+        flight["attributes"].pop("user")
+        flight_details.append(flight)
+
+    flight_response = {"data": flight_details}
+
+    return JsonResponse(flight_response, status=200)
+
+
+# Class-based view for using JSON-API approach to serialization
+
+# class FlightList(APIView):
+#     resource_name = "flight"
+
+#     def get(self, request, id):
+#         try:
+#             user = User.objects.get(pk=id)
+#         except User.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+
+#         flights = Flight.objects.filter(user=user)
+#         serializer = FlightSerializer(flights, many=True)
+#         return Response(serializer.data)
