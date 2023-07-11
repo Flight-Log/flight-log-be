726107dce1639b3836f403fabfb4b9b0d77469da
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-11 13:33:56 -0400

    chore: add commented out configuration for JSON-API library for reference only

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "rest_framework",
+    "rest_framework_json_api",
     "flight_log_be",
     "psycopg2",
     "django.contrib.admin",
@@ -140,3 +141,38 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# # REST Framework Settings for REST Framework JSON API
+# REST_FRAMEWORK = {
+#     "PAGE_SIZE": 10,
+#     "EXCEPTION_HANDLER": "rest_framework_json_api.exceptions.exception_handler",
+#     "DEFAULT_PAGINATION_CLASS": "rest_framework_json_api.pagination.JsonApiPageNumberPagination",
+#     "DEFAULT_PARSER_CLASSES": (
+#         "rest_framework_json_api.parsers.JSONParser",
+#         "rest_framework.parsers.FormParser",
+#         "rest_framework.parsers.MultiPartParser",
+#     ),
+#     "DEFAULT_RENDERER_CLASSES": (
+#         "rest_framework_json_api.renderers.JSONRenderer",
+#         # If you're performance testing, you will want to use the browseable API
+#         # without forms, as the forms can generate their own queries.
+#         # If performance testing, enable:
+#         # "example.utils.BrowsableAPIRendererWithoutForms",
+#         # Otherwise, to play around with the browseable API, enable:
+#         "rest_framework_json_api.renderers.BrowsableAPIRenderer",
+#     ),
+#     "DEFAULT_METADATA_CLASS": "rest_framework_json_api.metadata.JSONAPIMetadata",
+#     "DEFAULT_SCHEMA_CLASS": "rest_framework_json_api.schemas.openapi.AutoSchema",
+#     # Comment this section out or add additional configuration
+#     # "DEFAULT_FILTER_BACKENDS": (
+#     #     "rest_framework_json_api.filters.QueryParameterValidationFilter",
+#     #     "rest_framework_json_api.filters.OrderingFilter",
+#     #     "rest_framework_json_api.django_filters.DjangoFilterBackend",
+#     #     "rest_framework.filters.SearchFilter",
+#     # ),
+#     "SEARCH_PARAM": "filter[search]",
+#     "TEST_REQUEST_RENDERER_CLASSES": (
+#         "rest_framework_json_api.renderers.JSONRenderer",
+#     ),
+#     "TEST_REQUEST_DEFAULT_FORMAT": "vnd.api+json",
+# }
