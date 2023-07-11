cc0401436390b88282750b361b6073b5b9b58520
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-07-11 13:55:45 -0400

    tests: create sad path testing for creation of flight

--- flight_log_be/tests/test_views.py
+++ flight_log_be/tests/test_views.py
@@ -21,3 +21,27 @@ class FlightCreationTestCase(TestCase):
         self.assertEqual(response.status_code, 201)
         
         self.assertTrue(Flight.objects.filter(user=user).exists())
+
+class FlightCreationSadPathTestCase(TestCase):        
+    def test_sad_path_flight_creation(self):
+        user = UserFactory()
+        
+        flight_data = {
+            'date': '2022-01-01',
+            'aircraft': 'Boeing 747',
+            'day_hours': 5,
+            'night_hours': 3,
+            'description': 'Test flight',
+        }
+    
+        response = self.client.post(f'/api/v1/users/{user.id}/flights/', flight_data)
+        self.assertEqual(response.status_code, 400)
+        expected_response = { "errors": 
+                                [
+                                    {'start_location': ['This field is required.'],
+                                    'end_location': ['This field is required.']}
+                                ]
+                            }
+        self.assertEqual(response.json(), expected_response)
+        
+        self.assertFalse(Flight.objects.filter(user=user).exists())
