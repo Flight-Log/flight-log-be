a738f6dd89518580cf0876719dba4345d19cc12c
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-07-11 14:27:01 -0400

    chore: remove unused import from views.py

--- flight_log_be/views.py
+++ flight_log_be/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
