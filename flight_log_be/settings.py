6fcb8110630a6f27ab7af81cc88ae14e3eb3c793
Katie Lonsdale <katie952@gmail.com>
2023-07-13 15:51:35 -0600

    allow all for cors

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -57,10 +57,11 @@ MIDDLEWARE = [
 ]
 
 # Restrict CORS access for all domains pre-deployment
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'https://flight-log-be-24cea5be4c8e.herokuapp.com',
-)
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#   'https://flight-log-be-24cea5be4c8e.herokuapp.com',
+# )
 
 ROOT_URLCONF = "flight_log_be.urls"
 
