e379ef70bd10902412c17d441ced8632594ac12b
Katie Lonsdale <katie952@gmail.com>
2023-07-14 13:29:10 -0600

    add /

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -64,9 +64,9 @@ MIDDLEWARE = [
 ALLOWED_HOSTS = ['flight-log-be-24cea5be4c8e.herokuapp.com']
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-  'http://localhost:19006',
-  'http://localhost:19000',
-  'https://flight-log-ui-hjawad22-flightlog.vercel.app',
+  'http://localhost:19006/',
+  'http://localhost:19000/',
+  'https://flight-log-ui-hjawad22-flightlog.vercel.app/',
 )
 
 ROOT_URLCONF = "flight_log_be.urls"
