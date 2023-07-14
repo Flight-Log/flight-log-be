9f0b198067ea1c09a98cdf344ae1e64e62ae02c0
Katie Lonsdale <katie952@gmail.com>
2023-07-14 14:39:53 -0600

    fix: path in test

--- flight_log_be/tests/test_views.py
+++ flight_log_be/tests/test_views.py
@@ -14,7 +14,7 @@ class GetUserCase(TestCase):
         c = Client()
 
     def test_user_is_returned(self):
-        response = c.get(f"/api/v1/users/{user.id}")
+        response = c.get(f"/api/v1/users/{user.id}/")
         self.assertEqual(response.status_code, 200)
 
         returned_user = json.loads(response.content)["data"]
@@ -25,7 +25,7 @@ class GetUserCase(TestCase):
         self.assertEqual(returned_user["attributes"]["last_name"], user.last_name)
 
     def test_error_is_returned(self):
-        response = c.get(f"/api/v1/users/104234")
+        response = c.get(f"/api/v1/users/104234/")
         self.assertEqual(response.status_code, 404)
 
         returned_error = json.loads(response.content)
