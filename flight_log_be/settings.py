b19e311aad29978180ff2b1943de9dd7b4554086
Katie Lonsdale <katie952@gmail.com>
2023-07-14 13:31:10 -0600

    allow all

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -61,12 +61,12 @@ MIDDLEWARE = [
 ]
 
 # Restrict CORS access for all domains pre-deployment
-ALLOWED_HOSTS = ['flight-log-be-24cea5be4c8e.herokuapp.com']
-CORS_ORIGIN_ALLOW_ALL = False
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-  'http://localhost:19006/',
-  'http://localhost:19000/',
-  'https://flight-log-ui-hjawad22-flightlog.vercel.app/',
+  'http://localhost:19006',
+  'http://localhost:19000',
+  'https://flight-log-ui-hjawad22-flightlog.vercel.app',
 )
 
 ROOT_URLCONF = "flight_log_be.urls"
