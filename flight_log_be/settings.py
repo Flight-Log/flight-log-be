3c3dbd52963dcd10a5c6d9fac369a7782ab553f7
Katie Lonsdale <katie952@gmail.com>
2023-07-14 13:25:48 -0600

    add 19000 local host

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -65,6 +65,7 @@ ALLOWED_HOSTS = ['flight-log-be-24cea5be4c8e.herokuapp.com']
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:19006',
+  'http://localhost:19000',
   'https://flight-log-ui-hjawad22-flightlog.vercel.app',
 )
 
