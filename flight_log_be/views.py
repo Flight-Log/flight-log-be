aedf67c8be46b703c31cebdaab92cb3f5cae0fe7
Katie Lonsdale <katie952@gmail.com>
2023-07-15 16:17:11 -0600

    refactor: move logic into model methods

--- flight_log_be/views.py
+++ flight_log_be/views.py
@@ -12,60 +12,30 @@ from copy import deepcopy
 @api_view(["GET"])
 def return_a_user(request, id):
     try:
-        user = User.objects.get(id=id)
+        user = User.find_user(id)
     except:
-        error = {"errors": [{"detail": "User not found."}]}
-        return JsonResponse(error, status=404)
+        return JsonResponse(User.error_404(), status=404)
     serializer = UserSerializer(user)
-    user_details = {
-        "data": {"id": f"{user.id}", "type": "user", "attributes": serializer.data}
-    }
-    return JsonResponse(user_details, status=200)
+    return JsonResponse(User.serialize_user(serializer, user.id), status=200)
 
 
 def return_users(request):
-    users = User.objects.all()
+    users = User.all_users()
     serializer = UserSerializer(users, many=True)
-    user_details = []
-
-    for index, x in enumerate(users):
-        user = {
-            "id": f"{x.id}",
-            "type": "user",
-            "attributes": serializer.data[index],
-        }
-        user_details.append(user)
-
-    user_response = {"data": user_details}
-
-    return JsonResponse(user_response, status=200)
+    return JsonResponse(User.serialize_users(users, serializer), status=200)
 
 
 @api_view(["GET", "POST"])
 def return_flights(request, user):
     if request.method == "GET":
         try:
-            found_user = User.objects.get(pk=user)
+            found_user = User.find_user(user)
         except:
-            error = {"errors": [{"detail": "Invalid user id."}]}
-            return JsonResponse(error, status=404)
+            return JsonResponse(User.flight_error_404(), status=404)
 
-        flights = Flight.objects.filter(user=found_user)
+        flights = Flight.find_flights(found_user)
         serializer = FlightSerializer(flights, many=True)
-        flight_details = []
-
-        for index, x in enumerate(flights):
-            flight = {
-                "id": f"{x.id}",
-                "type": "flight",
-                "attributes": serializer.data[index],
-            }
-            flight["attributes"].pop("user")
-            flight_details.append(flight)
-
-        flight_response = {"data": flight_details}
-
-        return JsonResponse(flight_response, status=200)
+        return JsonResponse(Flight.serialize_flights(flights, serializer), status=200)
 
     elif request.method == "POST":
         data = deepcopy(request.data)
@@ -74,17 +44,9 @@ def return_flights(request, user):
 
         if serializer.is_valid():
             serializer.save()
-            flight_data = {
-                "data": {
-                    "id": Flight.objects.last().id,
-                    "type": "flight",
-                    "attributes": serializer.data,
-                }
-            }
-            return Response(flight_data, status=status.HTTP_201_CREATED)
+            return Response(Flight.serialize_flight(serializer), status=status.HTTP_201_CREATED)
         else:
-            error = {"errors": [serializer.errors]}
-            return Response(error, status=status.HTTP_400_BAD_REQUEST)
+            return Response(Flight.error_400(serializer), status=status.HTTP_400_BAD_REQUEST)
 
 
 # Class-based view for using JSON-API approach to serialization
