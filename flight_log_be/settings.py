770c1e3ada9c8928824d85ef0be9ee2295934abe
Caroline Peri <caroline.anne.peri@gmail.com>
2023-07-15 20:35:20 -0400

    fix: revert CORS settings to allow all hosts and allow all to TRUE for FE testing purposes

--- flight_log_be/settings.py
+++ flight_log_be/settings.py
@@ -61,14 +61,15 @@ MIDDLEWARE = [
 ]
 
 # Restrict CORS access for all domains pre-deployment
-ALLOWED_HOSTS = ['https://flight-log-be-24cea5be4c8e.herokuapp.com']
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:19006',
-  'http://localhost:19000',
-  'https://flight-log-ui-hjawad22-flightlog.vercel.app',
-  'https://flight-log-eight.vercel.app',
-)
+# ALLOWED_HOSTS = ["https://flight-log-be-24cea5be4c8e.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     "http://localhost:19006",
+#     "http://localhost:19000",
+#     "https://flight-log-ui-hjawad22-flightlog.vercel.app",
+#     "https://flight-log-eight.vercel.app",
+# )
 
 ROOT_URLCONF = "flight_log_be.urls"
 
